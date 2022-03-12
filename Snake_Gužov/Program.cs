89426d4c1ac0e1242fc5ccf6812d33f1de2189dd
Infoman724 <93324312+Infoman724@users.noreply.github.com>
2022-03-13 00:27:44 +0200

    Добавьте файлы проекта.

---
+++ Snake_Gužov/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Snake_Gužov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
