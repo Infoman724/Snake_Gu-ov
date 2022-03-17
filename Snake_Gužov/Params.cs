f6355da6bd19c4cf302297cca255d74d9a4d5171
Infoman724 <93324312+Infoman724@users.noreply.github.com>
2022-03-17 19:09:30 +0200

    в процессе добавления звуков и немного двигаемся по очкам

---
+++ Snake_Gužov/Params.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Snake_Gužov
+{
+    class Params
+    {
+        private string resourcesFoler;
+
+        public Params()
+        {
+            var ind = Directory.GetCurrentDirectory().ToString()                
+                  .IndexOf("bin", StringComparison.Ordinal); // Nony4uTb NHAEKC nanKn bin
+            string binFolder =
+               Directory.GetCurrentDirectory().ToString().Substring(0, ind)
+                    .ToString(); // путь до указанной в инкесе папки
+            resourcesFoler = binFolder + "resources\\";
+        }
+        public string GetResourceFolder()
+        {
+            return resourcesFoler;
+        }    
+    }
+}
